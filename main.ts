ef218fa29713968751a442d59cfa710b05e760a2
asianspiderman6 <113910625+asianspiderman6@users.noreply.github.com>
2022-09-19 09:56:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    if (_switch == 0) {
+        Number_1 += 1
+        basic.showNumber(Number_1)
+        if (_switch == 1) {
+            Number_1 += 1
+        }
+        basic.showNumber(0)
+    }
+})
+let _switch = 0
+let Number_1 = 0
+Number_1 = 0
+let Number_2 = 0
+_switch = 0
 basic.forever(function () {
 	
 })
